f133a2f682d84b34f7e35d8975a3b461e845a9bd
Thomas Baumann <tomvodi@protonmail.com>
2024-08-04 13:24:23 +0200

    moved and added test application and plugin

---
+++ cmd/plugin/implementation.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/tomvodi/limepipes-plugin-api/plugin/v1/interfaces"
+	"github.com/tomvodi/limepipes-plugin-api/plugin/v1/messages"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type impl struct {
+}
+
+func (i impl) ImportFile(filePath string) (*messages.ImportFileResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "ImportFile not implemented")
+}
+
+func (i impl) PluginInfo() (*messages.PluginInfoResponse, error) {
+	return &messages.PluginInfoResponse{
+		Name:        "Bagpipe Music Writer",
+		Description: "A plugin that opens Bagpipe Music Writer and Bagpipe Player files.",
+		Type:        messages.PluginType_IN,
+		FileTypes:   []string{"bww", "bmw"},
+	}, nil
+}
+
+func NewImplementation() interfaces.LimePipesPlugin {
+	return &impl{}
+}
---
+++ cmd/plugin/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/hashicorp/go-plugin"
+	"github.com/tomvodi/limepipes-plugin-api/plugin/v1/common"
+	"github.com/tomvodi/limepipes-plugin-api/plugin/v1/grpc_plugin"
+	"google.golang.org/grpc"
+)
+
+// defaultGRPCServer returns a new gRPC server with the given options.
+// Acts as a factory method for gRPC servers.
+func defaultGRPCServer(opts []grpc.ServerOption) *grpc.Server {
+	return grpc.NewServer(opts...)
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: common.HandshakeConfig,
+		Plugins: map[string]plugin.Plugin{
+			"bww": grpc_plugin.NewGrpcPlugin(NewImplementation()),
+		},
+
+		GRPCServer: defaultGRPCServer,
+	})
+}

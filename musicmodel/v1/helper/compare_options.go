b36b2c58b862913f4cf368677bea0a8d115fa1a5
Thomas Baumann <tomvodi@protonmail.com>
2024-08-08 15:37:05 +0200

    added types for convenience

---
+++ musicmodel/v1/helper/compare_options.go
@@ -0,0 +1,29 @@
+package helper
+
+import (
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/barline"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/measure"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols/embellishment"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols/movement"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols/timeline"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols/tuplet"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/tune"
+)
+
+var MusicModelCompareOptions = cmpopts.IgnoreUnexported(
+	tune.Tune{},
+	measure.Measure{},
+	measure.TimeSignature{},
+	measure.ImportMessage{},
+	symbols.Symbol{},
+	symbols.Note{},
+	tune.Tune{},
+	symbols.Rest{},
+	embellishment.Embellishment{},
+	tuplet.Tuplet{},
+	movement.Movement{},
+	timeline.TimeLine{},
+	barline.Barline{},
+)
---
+++ musicmodel/v1/musicmodel/musicmodel.go
@@ -0,0 +1,7 @@
+package musicmodel
+
+import (
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/tune"
+)
+
+type MusicModel []*tune.Tune

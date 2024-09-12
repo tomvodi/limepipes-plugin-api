79600403481449a65632ec815a7bb7f3cb0fe788
Thomas Baumann <tomvodi@protonmail.com>
2024-09-12 13:45:35 +0200

    update grpc plugin implementation

--- plugin/v1/interfaces/LimepipesPlugin.go
+++ plugin/v1/interfaces/LimepipesPlugin.go
@@ -1,9 +1,14 @@
 package interfaces
 
-import "github.com/tomvodi/limepipes-plugin-api/plugin/v1/messages"
+import (
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/tune"
+	"github.com/tomvodi/limepipes-plugin-api/plugin/v1/messages"
+)
 
 type LimePipesPlugin interface {
 	PluginInfo() (*messages.PluginInfoResponse, error)
 	ImportLocalFile(filePath string) (*messages.ImportFileResponse, error)
 	Import(data []byte) (*messages.ImportFileResponse, error)
+	ExportToLocalFile(tunes []*tune.Tune, filepath string) error
+	Export(tunes []*tune.Tune) ([]byte, error)
 }

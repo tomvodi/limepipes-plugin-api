5bb6a062f353703a9d4c22d28678dbb0b7b790e0
Thomas Baumann <tomvodi@protonmail.com>
2024-09-27 09:59:11 +0200

    changed methods on measure and note

--- musicmodel/v1/measure/measure_test.go
+++ musicmodel/v1/measure/measure_test.go
@@ -2,56 +2,8 @@ package measure
 
 import (
 	. "github.com/onsi/gomega"
-	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/barline"
-	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols"
 	"testing"
 )
-
-func TestMeasure_IsNil(t *testing.T) {
-	type fields struct {
-		LeftBarline    *barline.Barline
-		RightBarline   *barline.Barline
-		Time           *TimeSignature
-		Symbols        []*symbols.Symbol
-		Comments       []string
-		InlineText     []string
-		ParserMessages []*ParserMessage
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   bool
-	}{
-		{
-			name:   "empty",
-			fields: fields{},
-			want:   true,
-		},
-		{
-			name: "non-empty",
-			fields: fields{
-				LeftBarline: &barline.Barline{},
-			},
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			x := &Measure{
-				LeftBarline:    tt.fields.LeftBarline,
-				RightBarline:   tt.fields.RightBarline,
-				Time:           tt.fields.Time,
-				Symbols:        tt.fields.Symbols,
-				Comments:       tt.fields.Comments,
-				InlineText:     tt.fields.InlineText,
-				ParserMessages: tt.fields.ParserMessages,
-			}
-			if got := x.IsNil(); got != tt.want {
-				t.Errorf("IsNil() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestMeasure_AddMessage(t *testing.T) {
 	g := NewGomegaWithT(t)
--- musicmodel/v1/symbols/symbol.go
+++ musicmodel/v1/symbols/symbol.go
@@ -1,13 +1,141 @@
 package symbols
 
+import (
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols/accidental"
+	"github.com/tomvodi/limepipes-plugin-api/musicmodel/v1/symbols/tie"
+)
+
 func (x *Symbol) IsNote() bool {
 	return x.Note != nil
 }
 
 func (x *Symbol) IsValidNote() bool {
-	if x.Note != nil {
-		return x.Note.IsValid()
+	return x.Note.IsValid()
+}
+
+func (x *Symbol) CanBeMergedWith(other *Symbol) bool {
+	if x.isOnlyEmbellishment() && other.IsValidNote() {
+		return true
+	}
+
+	if x.isOnlyMovement() && other.IsValidNote() {
+		return true
+	}
+
+	if x.IsValidNote() && other.IsOnlyDots() {
+		return true
+	}
+
+	if x.IsValidNote() && other.IsOnlyFermata() {
+		return true
+	}
+
+	// tie start is merged with the next note
+	if x.IsOnlyTieStart() && other.IsValidNote() {
+		return true
+	}
+
+	// tie end is merged with the previous note
+	if x.IsValidNote() && other.IsOnlyTieEnd() {
+		return true
+	}
+
+	if x.isOnlyAccidental() && other.IsValidNote() {
+		return true
 	}
 
 	return false
+}
+
+func (x *Symbol) MergeWith(other *Symbol) {
+	if x.isOnlyEmbellishment() && other.IsValidNote() {
+		other.Note.Embellishment = x.Note.Embellishment
+		x.Note = other.Note
+	}
+
+	if x.isOnlyMovement() && other.IsValidNote() {
+		other.Note.Movement = x.Note.Movement
+		x.Note = other.Note
+	}
+
+	if x.isOnlyAccidental() && other.IsValidNote() {
+		other.Note.Accidental = x.Note.Accidental
+		x.Note = other.Note
+	}
+
+	if x.IsValidNote() && other.IsOnlyDots() {
+		x.Note.Dots = other.Note.Dots
+	}
+
+	// tie start is merged with the next note
+	if x.IsOnlyTieStart() && other.IsValidNote() {
+		other.Note.Tie = x.Note.Tie
+		x.Note = other.Note
+	}
+
+	// tie end is merged with the previous note
+	if x.IsValidNote() && other.IsOnlyTieEnd() {
+		x.Note.Tie = other.Note.Tie
+	}
+
+	if x.IsValidNote() && other.IsOnlyFermata() {
+		x.Note.Fermata = other.Note.Fermata
+	}
+}
+
+func (x *Symbol) isOnlyEmbellishment() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() && x.Note.Embellishment != nil
+}
+
+func (x *Symbol) isOnlyMovement() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() && x.Note.Movement != nil
+}
+
+func (x *Symbol) isOnlyAccidental() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() &&
+		x.Note.Accidental != accidental.Accidental_NoAccidental
+}
+
+func (x *Symbol) IsOnlyDots() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() && x.Note.Dots != 0
+}
+
+func (x *Symbol) IsOnlyFermata() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() && x.Note.Fermata
+}
+
+func (x *Symbol) IsOnlyTieStart() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() && x.Note.Tie == tie.Tie_Start
+}
+
+func (x *Symbol) IsOnlyTieEnd() bool {
+	if x.Note == nil {
+		return false
+	}
+
+	return !x.Note.IsValid() && x.Note.Tie == tie.Tie_End
 }

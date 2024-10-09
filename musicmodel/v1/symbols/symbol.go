b298faa684762fbdaeab13a2154f2536c207f43c
Thomas Baumann <tomvodi@protonmail.com>
2024-10-09 14:04:02 +0200

    removed bww plugin specific code

--- musicmodel/v1/symbols/symbol.go
+++ musicmodel/v1/symbols/symbol.go
@@ -13,77 +13,7 @@ func (x *Symbol) IsValidNote() bool {
 	return x.Note.IsValid()
 }
 
-func (x *Symbol) CanBeMergedWith(other *Symbol) bool {
-	if x.isOnlyEmbellishment() && other.IsValidNote() {
-		return true
-	}
-
-	if x.isOnlyMovement() && other.IsValidNote() {
-		return true
-	}
-
-	if x.IsValidNote() && other.IsOnlyDots() {
-		return true
-	}
-
-	if x.IsValidNote() && other.IsOnlyFermata() {
-		return true
-	}
-
-	// tie start is merged with the next note
-	if x.IsOnlyTieStart() && other.IsValidNote() {
-		return true
-	}
-
-	// tie end is merged with the previous note
-	if x.IsValidNote() && other.IsOnlyTieEnd() {
-		return true
-	}
-
-	if x.isOnlyAccidental() && other.IsValidNote() {
-		return true
-	}
-
-	return false
-}
-
-func (x *Symbol) MergeWith(other *Symbol) {
-	if x.isOnlyEmbellishment() && other.IsValidNote() {
-		other.Note.Embellishment = x.Note.Embellishment
-		x.Note = other.Note
-	}
-
-	if x.isOnlyMovement() && other.IsValidNote() {
-		other.Note.Movement = x.Note.Movement
-		x.Note = other.Note
-	}
-
-	if x.isOnlyAccidental() && other.IsValidNote() {
-		other.Note.Accidental = x.Note.Accidental
-		x.Note = other.Note
-	}
-
-	if x.IsValidNote() && other.IsOnlyDots() {
-		x.Note.Dots = other.Note.Dots
-	}
-
-	// tie start is merged with the next note
-	if x.IsOnlyTieStart() && other.IsValidNote() {
-		other.Note.Tie = x.Note.Tie
-		x.Note = other.Note
-	}
-
-	// tie end is merged with the previous note
-	if x.IsValidNote() && other.IsOnlyTieEnd() {
-		x.Note.Tie = other.Note.Tie
-	}
-
-	if x.IsValidNote() && other.IsOnlyFermata() {
-		x.Note.Fermata = other.Note.Fermata
-	}
-}
-
-func (x *Symbol) isOnlyEmbellishment() bool {
+func (x *Symbol) IsOnlyEmbellishment() bool {
 	if x.Note == nil {
 		return false
 	}
@@ -91,7 +21,7 @@ func (x *Symbol) isOnlyEmbellishment() bool {
 	return !x.Note.IsValid() && x.Note.Embellishment != nil
 }
 
-func (x *Symbol) isOnlyMovement() bool {
+func (x *Symbol) IsOnlyMovement() bool {
 	if x.Note == nil {
 		return false
 	}
@@ -99,7 +29,7 @@ func (x *Symbol) isOnlyMovement() bool {
 	return !x.Note.IsValid() && x.Note.Movement != nil
 }
 
-func (x *Symbol) isOnlyAccidental() bool {
+func (x *Symbol) IsOnlyAccidental() bool {
 	if x.Note == nil {
 		return false
 	}
